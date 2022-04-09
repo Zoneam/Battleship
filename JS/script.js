f3f7dc53757568472724dee29fe1c9b4538f13e2
Zoneam <zoneam@gmail.com>
2022-04-08 22:52:54 -0700

    added random starting point generator function

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,52 @@
 const userGameTable = $('#user-game-table');
-
+const aiGameTable = $('#ai-game-table');
+const emptyGameArray = new Array(10).fill(new Array(10).fill('-'));
+console.log(emptyGameArray);
 
 function generateGameBoard(player) {
     for(i = 0; i < 10; i++) {
         $(`#${player}-game-table`).append(`<tr class='' id="${player + '-' + 'row' + '-' + (i * 10)}"></tr>`);
         for(j = 0; j < 10; j++) {
-            $("tr").last().append(`<th class='tg-0lax' id="${(i * 10 + j)}"></th>`);
-        }
-    }
-}
-generateGameBoard('user')
+            $("tr").last().append(`<th class='tg-0lax' id="${player + '-' + (i * 10 + j)}"></th>`);
+        };
+    };
+};
+generateGameBoard('user');
+generateGameBoard('ai');
 
-generateGameBoard('ai')
+// Attach click event listeners
+$("#start").one("click", function(){
+    userGameTable.on('click', 'th', function() {
+        console.log("User tile# " + $(this).attr('id'));
+        $(this).css('background-color','green');
+    });
+
+    aiGameTable.on('click', 'th', function() {
+        console.log("Ai tile# " + $(this).attr('id'));
+        $(this).css('background-color','red');
+    });
+})
+
+//----------------------------------------------------------------
+// generating battleships
+function generateShips(){
+const arrayOfShips = [];
+const shipSizes = [1,2,3,3,4,5];
+const randomPlacementPosition = Math.floor(Math.random() * 100);
+console.log(randomPlacementPosition)
+console.log(Math.floor(randomPlacementPosition / 10), randomPlacementPosition % 10);
+
+
+$(`#ai-${randomPlacementPosition}`).css('background-color','red');
+    for(i = 0; i < shipSizes.length; i++) {
+        let isVertical = Math.random() < 0.5;
+        let ship = [];
+        for(j = 0; j < shipSizes[i]; j++){
+            ship.push('+');
+        }
+        arrayOfShips.push(ship);
+    }
+    console.log(arrayOfShips)
+}
+
+generateShips()
