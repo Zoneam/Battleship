1e5bb586ad9ec6984e9934a1b1a7b2a89f8f49d2
zoneam <zoneam@gmail.com>
2022-04-09 16:01:57 -0700

    random horizontal and vertical ship generation

--- JS/script.js
+++ JS/script.js
@@ -31,97 +31,100 @@ $("#start").one("click", function(){
 // generating battleships
 function generateShips(){
     const arrayOfShips = [];
-    const shipSizes = [1, 2, 3, 3, 4, 5];
+    const shipSizes = [1, 2, 2, 3, 4, 5];
     let randomLocation = null;
-    const occupiedPositions = [];
+    let occupiedPositions = [];
     let foundShip = false;
     let ship = [];
-    let isVertical;
-// console.log(randomPlacementPosition())
-// console.log(Math.floor(randomPlacementPosition / 10), randomPlacementPosition % 10);
-// $(`#ai-${randomPlacementPosition}`).css('background-color', 'red');
-    
-
+    let isHorizontal;
+    let containsTiles = false;
     for (let i = 0; i < shipSizes.length; i++) {
         console.log("-----------------------------------------")
         console.log("inside Loop:i=", i)
-        isVertical = Math.random() < 0.5;
+        isHorizontal = Math.random() < 0.5;
         ship = [];
         // first 1X1 square placement
         if (i === 0) {
-            let firstPosition = randomPlacementPosition(shipSizes[0], false); // 100 is board array length
+            let firstPosition = generateShipAndPosition(shipSizes[0], false); // 100 is board array length
             ship.push(firstPosition);
-            occupiedPositions.push(firstPosition);
-            console.log("first position:" , firstPosition)
+            occupiedPositions.push(firstPosition[0]);
+            console.log("first position:" , firstPosition[0])
         } else {
-            console.log("shipSize: ",shipSizes[i])
-            console.log("inside else:")
-            do {
-                for (let j = 0; j < shipSizes[i]; j++) {
-                        console.log("shipSizes",shipSizes[i])
-                        console.log("while")
-                        console.log("j = ", j)
-                        console.log("isVertical = ", isVertical)
-                        // random location where our new ship can be placed away from walls
-                        randomLocation = randomPlacementPosition(shipSizes[i], isVertical);
-                        //searching for horizontal ships
-                        console.log("randomLocation", randomLocation)
-                    // if (!occupiedPositions.includes(randomLocation) && !isVertical) {
-                        //function to find if can place not overlaping
-                        console.log("inside if:----")
-                        // for (let y = 0; y < shipSizes[i]; y++){
-                            // if (!occupiedPositions.includes(randomLocation + y)) {
-                                occupiedPositions.push(randomLocation) //??
-                                ship.push(randomLocation);//??
-                            // }
-                        // }
-                        // }
-                    }
-            } while (ship.length < shipSizes[i] )
-         
-        console.log("times", i)
+                
+           
+            console.log(" DOOOOOOOOOOOO")
+                // ship array comes back from our ship generator
+            randomLocation = generateShipAndPosition(shipSizes[i], isHorizontal);
+            containsTiles = randomLocation.some(position => {
+                return occupiedPositions.includes(position);
+            });
+                console.log("randomLocation:   ",randomLocation)
+                console.log("containsTiles: ", containsTiles)
+            if (!containsTiles) {
+                    //function to find if can place not overlaping
+                occupiedPositions = occupiedPositions.concat(randomLocation)
+                ship.push(randomLocation);
+            }
+
+
 
         }
-
         arrayOfShips.push(ship);
     }
-    colorizeShips(arrayOfShips);
+    colorizeShips(occupiedPositions);
     console.log("arrayOfShips",arrayOfShips);
     console.log("occupiedPositions: ",occupiedPositions)
 }
 
-const colorizeShips = (arrayOfShips) => {
-    arrayOfShips.forEach((ship, i) => {
-        ship.forEach((shipEL, j) => {
-            $(`#ai-${arrayOfShips[i][j]}`).css('background-color', 'red');
-        })
+const colorizeShips = (occupiedPositions) => {
+    occupiedPositions.forEach((ship, i) => {
+            $(`#ai-${occupiedPositions[i]}`).css('background-color', 'red');
     })
 }
 
 
-const randomPlacementPosition = (shipSize, isVertical) => {
-    let randomNumber = 0;
-    let horizontalAvailableNumbersArray = [];
+const generateShipAndPosition = (shipSize, isHorizontal) => {
+    console.log("isHorizontal: ",isHorizontal)
+    let generatedShip = [];
+    let horizontalShipArray = [];
+    let verticalShipArray = [];
     let persistNumber = 0;
-    if (!isVertical) {
-        randomNumber = Math.floor(Math.random() * (100 - shipSize - 1));
-        return randomNumber;
+    let randomLocation = 0;
+    console.log("Ship SZ:", shipSize)
+    console.log("-----------generating--------------")
+    // generating horizontal ships
+    if (!isHorizontal) {
+        console.log("verticalShipArray", verticalShipArray)
+        for (let i = 0; i < 100 - ((shipSize-1) * 10) ; i++) {
+                // adding all numbers except ship size that will hit bottom wall
+                horizontalShipArray.push((persistNumber));
+                persistNumber++
+        }
+        console.log("horizontalShipArray", horizontalShipArray)
+        randomLocation = horizontalShipArray[Math.floor(Math.random() * (100 - (shipSize * 10)))]; // -1 ??
+        for (let i = 0; i < shipSize; i++){
+            generatedShip.push(randomLocation + i * 10);
+        }
+        return generatedShip;
     }
-    if (isVertical) {
-        for (i = 0; i < 10; i++) {
-            for (j = 0; j < 10; j++) {
-                // adding all numbers except ship size that will hit right wall
+
+    if (isHorizontal) {
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                // adding all numbers except ship size that will hit bottom wall
                 if (j <= 10 - shipSize) {
-                    horizontalAvailableNumbersArray.push((persistNumber));
+                    horizontalShipArray.push((persistNumber));
                 }
                 persistNumber++
             }
         }
-        randomNumber = horizontalAvailableNumbersArray[ Math.floor(Math.random() * (100 - (shipSize * 10)))];
-        return randomNumber;
+        console.log("horizontalShipArray", horizontalShipArray)
+        randomLocation = horizontalShipArray[Math.floor(Math.random() * (100 - (shipSize * 10)))]; // -1 ??
+        for (let i = 0; i < shipSize; i++){
+            generatedShip.push(randomLocation + i);
+        }
+        return generatedShip;
     }
-    
-    
 }
 
 
