ae7ef9e22d976b04a62abe98236159580793d847
zoneam <zoneam@gmail.com>
2022-04-10 01:36:43 -0700

    blocks keep distance when generated

--- JS/script.js
+++ JS/script.js
@@ -6,8 +6,10 @@ const shipSizes = [1, 2, 2, 3, 4, 5];
 let moreHorizontalOrVertical = 0.5;
 // const emptyGameArray = new Array(10).fill(new Array(10).fill('-'));
 
-// Game Start
+// Game Init / restart
 function gameStart() {
+    generateShipsForUserButton.prop("disabled", false);
+    $("#start").prop("disabled", true);
     drawGameBoard('user');
     drawGameBoard('ai');
     generateShips('ai');
@@ -15,11 +17,15 @@ function gameStart() {
 
 
 generateShipsForUserButton.on('click', function () {
-        generateShips('user');
+    $("#start").prop("disabled", false);
+    generateShips('user');
+    
 });
 
 // Attach click event listeners
+
 $("#start").one("click", function () {
+    generateShipsForUserButton.prop("disabled", true);
     userGameTable.on('click', 'th', handleUserBoardClick);
     aiGameTable.on('click', 'th', handleAiBoardClick)
 });
@@ -54,8 +60,11 @@ function generateShips(player){
     let isHorizontal;
     let containsTiles;
     let firstPosition;
+    let touchingOthers;
+    let suroundCheckArray = [];
     for (let i = 0; i < shipSizes.length; i++) {
-        console.log("inside Loop:i=", i)
+        touchingOthers = true;
+        console.log("inside Loop:i=", i);
         isHorizontal = Math.random() <= moreHorizontalOrVertical;
         ship = [];
         // first 1X1 square placement
@@ -63,50 +72,70 @@ function generateShips(player){
             firstPosition = generateShipAndPosition(shipSizes[0], false); // 100 is board array length
             ship.push(firstPosition);
             occupiedPositions.push(firstPosition[0]);
-            console.log("first position:" , firstPosition[0])
+            console.log("first position:", firstPosition[0]);
         } else {
+            console.log("----------------------------------------------: ")
             randomLocation = generateShipAndPosition(shipSizes[i], isHorizontal);
             containsTiles = randomLocation.some(position => {
                 return occupiedPositions.includes(position);
             });
             //function to find if can place not overlaping
-            while (containsTiles) {
+            while ((touchingOthers === true) || (containsTiles === true)) {
+                console.log("00000000000000000000000000000000000000000000")
+                suroundCheckArray = [];
                 randomLocation = generateShipAndPosition(shipSizes[i], isHorizontal);
-                containsTiles = randomLocation.some(position => {
-                    return occupiedPositions.includes(position);
+                
+                containsTiles = occupiedPositions.some(position => {
+                    return randomLocation.includes(position);
                 });
-            }
+                console.log("randomLocation", randomLocation)
+                console.log("occupiedPositions: ", occupiedPositions)
+                console.log("containsTiles", containsTiles)
+                
+                randomLocation.forEach(el => {
+                    suroundCheckArray.push(el + 1, el - 1, el + 10, el - 10)
+                });
+
+                console.log("suroundCheckArray", suroundCheckArray)
+                
+                console.log("----------ForEach----------- ")
+               
+                touchingOthers = suroundCheckArray.some(el => {
+                    return occupiedPositions.includes(el);
+                })
+        
+                console.log("touchingOthers0000000000000000",touchingOthers)
+            };
                 occupiedPositions = occupiedPositions.concat(randomLocation)
                 ship.push(randomLocation);
-                console.log("occupiedPositions length", occupiedPositions.length)
-                console.log("occupiedPositions: ",occupiedPositions)
-                console.log("randomLocation:   ", randomLocation)
-                console.log("containsTiles: ", containsTiles)
-        }
+            //     console.log("occupiedPositions length", occupiedPositions.length)
+            //     // console.log("occupiedPositions: ",occupiedPositions)
+            //     console.log("randomLocation:   ", randomLocation)
+            // console.log("containsTiles: ", containsTiles)
+            
+        }; 
         arrayOfShips.push(ship);
     }
     colorizeShips(occupiedPositions,player);
     console.log("arrayOfShips",arrayOfShips);
-    console.log("occupiedPositions: ",occupiedPositions)
+    console.log("occupiedPositions Final: ",occupiedPositions)
 };
 
 // To Generate random ships
 const generateShipAndPosition = (shipSize, isHorizontal) => {
-    console.log("isHorizontal: ", isHorizontal)
     let generatedShip = [];
     let horizontalShipArray = [];
     let verticalShipArray = [];
     let persistNumber = 0;
     let randomLocation = 0;
-    console.log("--generating--")
     // if generating vertical ships
     if (!isHorizontal) {
         for (let i = 0; i < 100 - ((shipSize - 1) * 10); i++) {
             // adding all numbers except ship size that will hit bottom wall
-            horizontalShipArray.push((persistNumber));
+            verticalShipArray.push((persistNumber));
             persistNumber++
         }
-        randomLocation = horizontalShipArray[Math.floor(Math.random() * (100 - ((shipSize - 1) * 10)))]; // -1 ??
+        randomLocation = verticalShipArray[Math.floor(Math.random() * (100 - ((shipSize - 1) * 10)))]; // -1 ??
         for (let i = 0; i < shipSize; i++) {
             generatedShip.push(randomLocation + i * 10);
         }
